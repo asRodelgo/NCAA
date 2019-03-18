7f868b8c3d8d8e117312d9c3b97cf9dec72dc43b
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2019-03-18 10:26:30 -0400

    ready for submission

--- ncaa2019_extra.R
+++ ncaa2019_extra.R
@@ -10,11 +10,11 @@ library(tidyverse)
 # Read data
 teams <- read.csv("data/data2019/Teams.csv", stringsAsFactors = FALSE)
 regSeason <- read.csv("data/data2019/RegularSeasonCompactResults.csv")
-thisSeason <- filter(regSeason, Season == 2019)
+thisSeason <- filter(regSeason, Season == 2018)
 thisSeasonTeams <- unique(thisSeason$WTeamID)
 tourneyResults <- read.csv("data/data2019/NCAATourneyCompactResults.csv")
 tourneySeeds <- read.csv("data/data2019/NCAATourneySeeds.csv")
-thisTourneySeeds <- filter(tourneySeeds, Season == 2019)
+thisTourneySeeds <- filter(tourneySeeds, Season == 2018)
 selectedTeams <- thisTourneySeeds$Team
 #
 # Read players and events as well
@@ -40,7 +40,7 @@ team_stats_18 <- left_join(team_stats_18, loc_factor, by = c("TeamID" = "WTeamID
   mutate(HomeWinPerc = ifelse(is.na(HomeWinPerc),0,HomeWinPerc))
 
 set.seed(456)
-teamCluster <- kmeans(team_stats_18[,-c(1,2)], 5, nstart = 10, iter.max = 20)
+teamCluster <- kmeans(team_stats_18[,-c(1,2)], 10, nstart = 10, iter.max = 20)
 team_stats_18 <- bind_cols(team_stats_18, cluster = teamCluster$cluster)
 # add clusters to regSeason
 regSeason_18 <- filter(regSeason, Season == "2018") %>%
@@ -79,7 +79,7 @@ cluster_v_cluster <- group_by(team_stats_cluster2, team_cluster) %>%
   summarise_if(is.numeric, funs(avg_net_rtg = mean, sd_net_rtg = sd) , na.rm = TRUE)
 #
 team_stats_cluster3 <- gather(team_stats_cluster2, opp_cluster, net_rtg, -contains("team")) %>%
-  mutate(opp_cluster = as.numeric(opp_cluster))
+  mutate(opp_cluster = as.numeric(opp_cluster), net_rtg = as.numeric(str_replace_na(net_rtg, 0)))
 #
 # Compute Historical NCAA tournament strength per team
 # round of 64 > 1 point; round of 32 > 2 pts; ... ; champs > 7 pts.
@@ -117,8 +117,8 @@ for (i in 1:(length(selectedTeams)-1)) {
     thisTeamA <- filter(team_stats_cluster_all, team_id == selectedTeams[i])
     thisTeamB <- filter(team_stats_cluster_all, team_id == selectedTeams[j])
     if (nrow(thisTeamA)*nrow(thisTeamB) > 0){
-      muA <- thisTeamA$Off_rating + thisTeamB$Def_rating - mu + thisTeamA$tourneyPts - thisTeamB$tourneyPts
-      muB <- thisTeamB$Off_rating + thisTeamA$Def_rating - mu + thisTeamB$tourneyPts - thisTeamA$tourneyPts
+      muA <- thisTeamA$Off_rating[1] + thisTeamB$Def_rating[1] - mu + thisTeamA$tourneyPts[1] - thisTeamB$tourneyPts[1]
+      muB <- thisTeamB$Off_rating[1] + thisTeamA$Def_rating[1] - mu + thisTeamB$tourneyPts[1] - thisTeamA$tourneyPts[1]
       predict_mu[k,1] <- selectedTeams[i]
       predict_mu[k,2] <- selectedTeams[j]
       predict_mu[k,3] <- muA
@@ -185,14 +185,34 @@ submission2_prep <- left_join(predict_mu2, select(team_stats_cluster_all, team_i
   distinct(teamA, teamB, .keep_all = TRUE)
 # add cluster net ratings
 submission2_prep <- left_join(submission2_prep, team_stats_cluster3, by = c("team_A"="team_id","cluster_A"="team_cluster","cluster_B"="opp_cluster")) %>%
-  left_join(team_stats_cluster3, by = c("team_A"="team_id","cluster_B"="team_cluster","cluster_A"="opp_cluster")) %>%
+  left_join(team_stats_cluster3, by = c("team_B"="team_id","cluster_B"="team_cluster","cluster_A"="opp_cluster")) %>%
+  mutate(net_rtg_diff = net_rtg.x - net_rtg.y) %>%
+  select(-contains(".")) %>%
+  mutate(prob_clst = 1-pnorm(0,muA-muB+net_rtg_diff/10,sqrt(2)*sigma),
+         prob_final_clst = prob_clst - (rank_diff + seed_diff)/100) %>%
+  mutate(prob_final_clst = ifelse(prob_final_clst > 1, 1, ifelse(prob_final_clst < 0, 0, prob_final_clst))) %>%
+  ungroup() %>%
+  distinct(teamA, teamB, .keep_all = TRUE)
 
-
+submission2_onlyProbs <- select(submission2_prep, team_A, team_B, contains("prob")) %>%
+  distinct(team_A, team_B, .keep_all = TRUE)
+  
 submission2 <- arrange(submission2_prep,team_A,team_B) %>%
   left_join(teams,by=c("team_A"="TeamID")) %>%
   left_join(teams,by=c("team_B"="TeamID")) %>%
   mutate(Id = ifelse(team_A > team_B, paste("2018",team_B,team_A,sep = "_"),paste("2018",team_A,team_B,sep = "_")),
          Pred = ifelse(team_A > team_B,1-prob_final,prob_final)) %>%
+  #mutate(Pred = ifelse(Pred >=.9,.999,ifelse(Pred<=.1,0.001,Pred))) %>%
+  #select(Id,Pred,Team_Name_A = Team_Name.x, Team_Name_B = Team_Name.y) %>%
+  select(Id,Pred) %>%
+  distinct(Id, .keep_all = TRUE) %>%
+  arrange(Id)
+
+submission2clst <- arrange(submission2_onlyProbs,team_A,team_B) %>%
+  left_join(teams,by=c("team_A"="TeamID")) %>%
+  left_join(teams,by=c("team_B"="TeamID")) %>%
+  mutate(Id = ifelse(team_A > team_B, paste("2018",team_B,team_A,sep = "_"),paste("2018",team_A,team_B,sep = "_")),
+         Pred = ifelse(team_A > team_B,1-prob_final_clst,prob_final_clst)) %>%
   #mutate(Pred = ifelse(Pred >=.9,.999,ifelse(Pred<=.1,0.001,Pred))) %>%
   #select(Id,Pred,Team_Name_A = Team_Name.x, Team_Name_B = Team_Name.y) %>%
   select(Id,Pred) %>%
@@ -205,10 +225,27 @@ submission3 <- mutate(submission2, Pred = ifelse(Pred >=.95,.999,ifelse(Pred<=.0
   select(Id,Pred) %>%
   arrange(Id)
 
+submission3clst <- mutate(submission2clst, Pred = ifelse(Pred >=.95,.999,ifelse(Pred<=.05,0.001,Pred))) %>%
+  #select(Id,Pred,Team_Name_A = Team_Name.x, Team_Name_B = Team_Name.y) %>%
+  select(Id,Pred) %>%
+  arrange(Id)
 # write.csv(submission3, "data/ncaa2018_muyayo3.csv",row.names = FALSE)
 
 # compare submissions
-compare_subm <- inner_join(submission, submission2, by = "Id")
+compare_subm <- inner_join(submission, select(submission2, Id, Pred2 = Pred), by = "Id") %>%
+  inner_join(select(submission3, Id, Pred3 = Pred), by = "Id") %>%
+  inner_join(select(submission2clst, Id, Pred2clst = Pred), by = "Id") %>%
+  inner_join(select(submission3clst, Id, Pred3clst = Pred), by = "Id")
+
+ensemble_subm <- gather(compare_subm, Prediction, Value, -Id) %>%
+  group_by(Id) %>%
+  summarize(Pred = mean(Value))
+# compare with actual results
+actual_results <- filter(tourneyResults, Season == 2018) %>%
+  mutate(Id = ifelse(WTeamID > LTeamID, paste("2018",LTeamID,WTeamID,sep = "_"),paste("2018",WTeamID,LTeamID,sep = "_")),
+         outcome = ifelse(WTeamID > LTeamID, 0,1)) %>%
+  select(Id,outcome)
+compare_results <- inner_join(compare_subm,actual_results, by = "Id")
 
 # use this to fill out a bracket manually
 #write.csv(select(predict_mu2, teamName_A, teamName_B, prob), "teamNames_probs.csv",row.names = FALSE)
